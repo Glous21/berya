9dadb98b0680c32fb88b57ecf303c4f82925a617
Glous21 <Glous21@example.com>
2025-08-09 09:02:03 +0700

    Convert Express backend to Vercel serverless

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
+// api/index.js
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const serverless = require('serverless-http');
 require('dotenv').config();
 
+// Import Routes
 const serviceRoutes = require('../routes/serviceRoutes');
 const benefitRoutes = require('../routes/benefitRoutes');
 const testimonialRoutes = require('../routes/testimonialRoutes');
@@ -13,11 +15,16 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// API Routes
 app.use('/api/services', serviceRoutes);
 app.use('/api/benefits', benefitRoutes);
 app.use('/api/testimonials', testimonialRoutes);
 
-app.get('/', (req, res) => res.send('API is running...'));
+// Default Route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 
-// Export as serverless function
+// Export untuk Vercel (serverless function)
+module.exports = app;
 module.exports.handler = serverless(app);
